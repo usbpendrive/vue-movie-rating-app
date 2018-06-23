f8dfd6fabe0da00bb1c55be253fa397f4e7f1f6c
usbpendrive <ferry.fernando@gmail.com>
2018-06-23 11:37:49 +0700

    Add rating system. Prepare for authentication.

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,4 +1,5 @@
 const MovieSchema = require('../models/Movie');
+const Rating = require('../models/Rating');
 
 module.exports.controller = (app) => {
   app.get('/movies', (req, res) => {
@@ -12,6 +13,23 @@ module.exports.controller = (app) => {
     MovieSchema.findById(req.params.id, 'name description release_year genre', (error, movie) => {
       if (error) { console.error(error); }
       res.send(movie);
+    });
+  });
+
+  app.post('/movies/rate/:id', (req, res) => {
+    const rating = new Rating({
+      movie_id: req.params.id,
+      user_id: req.body.user_id,
+      rate: req.body.rate,
+    });
+
+    rating.save(function (error, rating) {
+      if (error) { console.log(error); }
+      res.send({
+        movie_id: rating.movie_id,
+        user_id: rating.user_id,
+        rate: rating.rate
+      });
     });
   });
 
--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -8,7 +8,7 @@
             <span class="grey--text">{{ movie.release_year }} ‧ {{ movie.genre }}</span>
           </div>
         </v-card-title>
-        <h6 class="card-title">Rate this movie</h6>
+        <h6 class="card-title" v-if="current_user" @click="rate">Rate this movie</h6>
         <v-card-text>
           {{ movie.description }}
         </v-card-text>
@@ -19,6 +19,30 @@
 
 <script>
 import axios from 'axios';
+import Vue from 'vue';
+import StarRating from 'vue-star-rating';
+
+const wrapper = document.createElement('div');
+const state = {
+  note: 0,
+};
+
+const RatingComponent = Vue.extend({
+  data() {
+    return { rating: 0 };
+  },
+  watch: {
+    rating(newVal) { state.note = newVal; },
+  },
+  template: `
+    <div class="rating">
+      How was your experience watching this movie?
+      <star-rating v-model="rating" :show-rating="false"></star-rating>
+    </div>`,
+  components: { 'star-rating': StarRating },
+});
+
+const component = new RatingComponent().$mount(wrapper);
 
 export default {
   name: 'Movie',
@@ -31,6 +55,35 @@ export default {
     this.fetchMovie();
   },
   methods: {
+    async rate() {
+      this.$swal({
+        content: component.$el,
+        buttons: {
+          confirm: {
+            value: 0,
+          },
+        },
+      }).then(() => {
+        const movieId = this.$route.params.id;
+        return axios({
+          method: 'post',
+          data: {
+            rate: state.note,
+          },
+          url: `http://localhost:8081/movies/rate/${movieId}`,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+          .then(() => {
+            this.$swal(`Thank you for rating! ${state.note}`, 'success');
+          })
+          .catch((error) => {
+            const message = error.responses.data.message;
+            this.$swal('Error!', `${message}`, 'error');
+          });
+      });
+    },
     async fetchMovie() {
       return axios({
         method: 'get',
