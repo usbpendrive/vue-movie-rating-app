36d39f6c0be34fdd3cb788e78ee55d2f4ffa7e32
usbpendrive <ferry.fernando@gmail.com>
2018-06-23 10:08:18 +0700

    Wiring movie controller with vue.

--- src/components/AddMovie.vue
+++ src/components/AddMovie.vue
@@ -4,14 +4,18 @@
                   :rules="nameRules" required></v-text-field>
     <v-text-field name="input-7-1" label="Movie Description"
                   v-model="description" multi-line></v-text-field>
-    <v-select label="Movie Release Year" v-model="release_year" :items="years"></v-select>
-    <v-text-field label="Movie Genre" v-model="genre"></v-text-field>
+    <v-select label="Movie Release Year" v-model="release_year"
+              :items="years" :rules="releaseRules"></v-select>
+    <v-text-field label="Movie Genre"
+                  v-model="genre" :rules="genreRules"></v-text-field>
     <v-btn @click="submit" :disabled="!valid">submit</v-btn>
     <v-btn @click="clear">clear</v-btn>
   </v-form>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: () => ({
     valid: true,
@@ -21,6 +25,13 @@ export default {
     release_year: '',
     nameRules: [
       v => !!v || 'Movie name is required',
+    ],
+    genreRules: [
+      v => !!v || 'Movie genre is required',
+      v => (v && v.length <= 80) || 'Genre must be less than or equals 80 characters',
+    ],
+    releaseRules: [
+      v => !!v || 'Movie release year is required',
     ],
     select: null,
     years: [
@@ -33,8 +44,26 @@ export default {
   methods: {
     submit() {
       if (this.$refs.form.validate()) {
-        // Perform next action
+        return axios({
+          method: 'post',
+          data: {
+            name: this.name,
+            description: this.description,
+            release_year: this.release_year,
+            genre: this.genre,
+          },
+          url: 'http://localhost:8081/movies',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+          .then(() => {
+            this.$router.push({ name: 'Home' });
+            this.$refs.form.reset();
+          })
+          .catch(() => {});
       }
+      return true;
     },
     clear() {
       this.$refs.form.reset();
